c0d6433048fd3d4008a6047ed80c646bd09226e9
Aries Yang <ariesssyyyang@gmail.com>
2020-06-24 00:57:31 +0800

    Initial commit

---
+++ RandomFood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomFood
+//
+//  Created by Aries Yang on 2020/6/24.
+//  Copyright © 2020 Aries Yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
