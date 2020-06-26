3d006b78891acf7d7795d4cf401f20f1f0f75ce4
Aries Yang <ariesssyyyang@gmail.com>
2020-06-27 02:16:16 +0800

    Add refresh control

--- RandomFood/ViewController.swift
+++ RandomFood/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     let tableView: UITableView = UITableView()
 
-    let foodList: [Food] = [
+    var foodList: [Food] = [
         Food(name: "1.  🍌 Banana", spicy: false, type: .undefined, budge: .cheap),
         Food(name: "2.  🍎 Apple", spicy: false, type: .undefined, budge: .cheap),
         Food(name: "3.  🥩 Steak", spicy: false, type: .us, budge: .expensive),
@@ -42,14 +42,38 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        // MARK: - Refresh Control
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(refreshData(_:)), for: .valueChanged)
+        refreshControl.attributedTitle = NSAttributedString(string: "Shuffling")
+        tableView.refreshControl = refreshControl
+
+        // MARK: - UI
         view.backgroundColor = .white
         tableView.backgroundColor = .white
         tableView.frame = view.bounds
         tableView.frame.origin.y = UIApplication.shared.statusBarFrame.height
         view.addSubview(tableView)
+
+        // MARK: - Data Source
         tableView.register(FoodCell.self, forCellReuseIdentifier: "\(FoodCell.self)")
         tableView.dataSource = self
         tableView.separatorStyle = .none
+    }
+}
+
+private extension ViewController {
+    @objc func refreshData(_ refreshControl: UIRefreshControl) {
+        shuffleFoodList {
+            self.tableView.reloadData()
+            refreshControl.endRefreshing()
+        }
+    }
+
+    func shuffleFoodList(completion: @escaping () -> Void) {
+        foodList.shuffle()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1, execute: completion)
     }
 }
 
